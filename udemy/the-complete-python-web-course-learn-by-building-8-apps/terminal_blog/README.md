2564d2e9a686cfc5511b6f4defad788f96302f78
Lamar Greene <github@lamargreene.com>
2017-07-08 13:35:28 -0600

    App complete
    This commit contains the files needed to run the app in the terminal.
    it was built in a virtual environment, but does not contain the
    environment files. (Built using python3)

---
+++ udemy/the-complete-python-web-course-learn-by-building-8-apps/terminal_blog/README.md
@@ -0,0 +1,9 @@
+# terminal_blog
+
+This is a simple app for the terminal that allows users to create blogs, and to read/write entries.
+
+I did mine a little different, I used an external mongodb implementation through mlab.com instead of installing directly on my system. (Since I can't install mongodb in a virtual environment, I thought this would be better. details are in my secure data file.)
+
+Instructors code for this segment can be found here... https://github.com/schoolofcode-me/terminal_blog
+
+(This app was built with python 3.6) 
--- udemy/the-complete-python-web-course-learn-by-building-8-apps/terminal_blog/app.py
+++ udemy/the-complete-python-web-course-learn-by-building-8-apps/terminal_blog/app.py
@@ -1,4 +1,33 @@
+from database import Database
 
-import pymongo
+from menu import Menu
 
-uri = ""
+Database.initialize()
+
+menu = Menu()
+
+menu.run_menu()
+
+# --------  stuff that has been removed when menu created  -------
+
+# blog = Blog(author='Lamar', title='Blog title', description='best blog')
+#
+# blog.new_post()
+#
+# blog.save_to_mongo()
+#
+# from_database=Blog.from_mongo(blog.id)
+#
+# print(blog.get_posts())
+# -----------
+# post = Post(blog_id="125",
+#             tLitle="Yet Another great post",
+#             content="content galore",
+#             author="Lamar")
+
+# post.save_to_mongo()
+
+#post = Post.from_mongo('ba8466fdd69947b981fd0fd74391805b')
+# post = Post.from_blog('125')
+#
+# print(post)
